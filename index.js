293a8c5114e5a013695ff8e6e8f6c301cd685dcd
Linkeo Lyon <studio@Mac-mini-de-Linkeo.local>
2017-05-22 09:45:05 +0200

    fix #3 and support git+ssh installed modules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var Q = require('q');
 
 var REGEX = {
-  package: /^([│ ]*)[└├+`][─-]+┬?\s+(.*)@(.*)$/,
+  package: /^([│ ]*)[└├+`][─-]+┬?\s+((?:@(?:[^\/]+?)[\/])?(?:[^\/]+?))@(\S*).*$/,
   invalid: /^(.*)\s+invalid$/,
   unmet: /^.*UNMET DEPENDENCY\s+(.*)$/,
   version: /^([\d.]*)(?:\s+->\s+(.*))$/
